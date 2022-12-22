358cb91e839a1e021b2b902ac6e64cd55912a63d
Rima Bhumbla <105192284+rbhumbla1@users.noreply.github.com>
2022-12-21 22:31:26 -0800

    added code to generate markdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,133 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'Apache 2.0':
+      license = 'Apache_2.0';
+      break;
+    case 'BSD 3-Clause':
+      license = 'BSD_3--Clause';
+      break;
+    case 'GPL 3.0':
+      license = 'GPL_3.0';
+      break;
+    default:
+      license = license;
+      break;
+  }
+  let badge = `![GitHub License](https://img.shields.io/badge/License-${license}-blue.svg)`;
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return 'https://opensource.org/licenses/MIT';
+      break;
+    case 'Apache 2.0':
+      return 'https://opensource.org/licenses/Apache-2.0';
+      break;
+    case 'GPL 3.0':
+      return 'https://opensource.org/licenses/GPL-3.0';
+      break;
+    case 'BSD 3-Clause':
+      return 'https://opensource.org/licenses/BSD-3-Clause';
+      break;
+    default:
+      return '';
+      break;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let lic = "";
+  if (license === 'None') {
+    lic = `## License\n
+  This project is not licensed.\n\n`;
+  }
+  else {
+    lic = `## License\n
+  This project is licensed under ${license} license.\n\n`;
+  }
+  return lic;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  console.log(data);
 
-`;
+  let content = `# ${data.title}\n\n`;
+
+  let badge = renderLicenseBadge(data.license);
+  let link = renderLicenseLink(data.license);
+  if (link !== '')
+    content += `[${badge}](${link})\n\n`;
+  else
+    content += `${badge}\n\n`;
+
+  let descr = `## Description\n
+  ${data.description}\n\n`;
+
+  content += descr;
+
+  let toc = "";
+
+  if (data.toc === 'yes') {
+    toc = `## Table of Contents\n
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)\n\n`;
+
+    content += toc;
+  }
+
+
+  let install = `## Installation\n
+  To install necessary dependencies, run the following command:
+  \`\`\`
+  ${data.installation}
+  \`\`\`\n\n`;
+
+  content += install;
+
+  let use = `## Usage\n
+  ${data.usage}\n\n`;
+
+  content += use;
+
+  let lic = renderLicenseSection(data.license);
+
+  content += lic;
+
+  let contribute = `## Contributing\n
+  ${data.contribution}\n`;
+
+  content += contribute;
+
+  let tests = `## Tests\n
+  To run tests, please run the following command:
+  \`\`\`
+  ${data.tests}
+  \`\`\`\n\n`;
+
+  content += tests;
+
+  let ques = `## Questions\n
+  If you have any questions about the repository and project, or would like to open an issue or would like to contact me for contributing or any other subject, you can do so at ${data.email}. You can welcome to see more ofmy work at ${data.username}. \n\n`;
+
+  content += ques;
+
+  return content;
+
 }
 
 module.exports = generateMarkdown;
+
+
